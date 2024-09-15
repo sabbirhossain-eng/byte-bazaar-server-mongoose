ea0494a1bd8ad753daddef0f3f1bef7ca86c7e9d
Sabbir Hossain <sabbirh8181@gmail.com>
2024-09-15 23:47:45 +0600

    user model updates

--- src/schema/userModel.js
+++ src/schema/userModel.js
@@ -2,11 +2,27 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
+        first_Name: {
+            type: String
+        },
+        last_Name: {
+            type: String
+        },
+        phone_Number: {
+            type: Number
+        },
         email: {
             type: String,
             unique : true,
             require: [true, 'A user must have a email']
-        }
+        },
+        role: {
+            type: String,
+            default: 'user',
+        },
+        image: {
+            type: String,
+        },
     },
 
 );
